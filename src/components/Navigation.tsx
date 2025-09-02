3236fb6de39f1dc0ac5bfecb44ab8e0a80dffb95
dlomojo <spoonie23@yahoo.com>
2025-09-02 17:13:17 -0400

    new file:   public/images/DS banner.png 	modified:   src/components/Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -24,11 +24,12 @@ const Navigation = () => {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <Link to="/" className="flex items-center space-x-2">
-            <div className="w-8 h-8 bg-blue-600 rounded-lg flex items-center justify-center">
-              <span className="text-white font-bold text-sm">DS</span>
-            </div>
-            <span className="font-bold text-xl text-slate-900">Detached Solution</span>
+          <Link to="/" className="flex items-center">
+            <img 
+              src="/images/DS Banner.png" 
+              alt="Detached Solution" 
+              className="h-10 w-auto"
+            />
           </Link>
 
           {/* Desktop Navigation */}
